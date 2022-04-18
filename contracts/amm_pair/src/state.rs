8660fb32f0af56809a3c1f0a0e812b1238eaf8da
Michal Sivak <sivakmichal@gmail.com>
2022-04-18 23:58:13 +1000

    amm_contract

--- contracts/amm_pair/src/state.rs
+++ contracts/amm_pair/src/state.rs
@@ -1,21 +1,88 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-use cosmwasm_std::{CanonicalAddr, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+use shadeswap_shared::{
+    fadroma::{
+        scrt_link::{ContractLink},
+    }
+    fadroma::
+    fadroma::scrt_addr::{Humanize, Canonize};
+    fadroma::scrt::{
+    Api, CanonicalAddr, Extern, HumanAddr, Uint128,
+    Querier, StdResult, Storage, StdError
+    }
+};
+use 
+use fadroma::scrt_storage::{load, save};
+}
+use serde::{Serialize,Deserialize};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
+use token::TokenPair;
+pub const BLOCK_SIZE: usize = 256;
+
+#[derive(Serialize, Deserialize,  PartialEq, Debug)]
+pub(crate) struct Config<A: Clone> {
+    pub factory_info:  ContractLink<A>,
+    pub lp_token_info: ContractLink<A>,
+    pub pair:          TokenPair<A>,
+    pub contract_addr: A,
+    pub viewing_key:ViewingKey,
 }
 
-pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
-    singleton(storage, CONFIG_KEY)
+
+impl Canonize<Config<CanonicalAddr>> for Config<HumanAddr> {
+    fn canonize (&self, api: &impl Api) -> StdResult<Config<CanonicalAddr>> {
+        Ok(Config {
+            factory_info:  self.factory_info.canonize(api)?,
+            lp_token_info: self.lp_token_info.canonize(api)?,
+            pair:          self.pair.canonize(api)?,
+            contract_addr: self.contract_addr.canonize(api)?,
+            viewing_key:   self.viewing_key.clone()
+        })
+    }
+}
+impl Humanize<Config<HumanAddr>> for Config<CanonicalAddr> {
+    fn humanize (&self, api: &impl Api) -> StdResult<Config<HumanAddr>> {
+        Ok(Config {
+            factory_info:  self.factory_info.humanize(api)?,
+            lp_token_info: self.lp_token_info.humanize(api)?,
+            pair:          self.pair.humanize(api)?,
+            contract_addr: self.contract_addr.humanize(api)?,
+            viewing_key:   self.viewing_key.clone()
+        })
+    }
 }
 
-pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
-    singleton_read(storage, CONFIG_KEY)
+
+pub(crate) fn store_config <S: Storage, A: Api, Q: Querier>(
+    deps:   &mut Extern<S, A, Q>,
+    config: &Config<HumanAddr>
+) -> StdResult<()> {
+    save(&mut deps.storage, CONFIG_KEY, &config.canonize(&deps.api)?)
 }
+
+pub(crate) fn load_config<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>
+) -> StdResult<Config<HumanAddr>> {
+    let result: Config<CanonicalAddr> = load(&deps.storage, CONFIG_KEY)?.ok_or(
+        StdError::generic_err("Config doesn't exist in storage.")
+    )?;
+    result.humanize(&deps.api)
+}
+
+
+pub mod swapdetails {
+    use super::*;
+
+    #[derive(Serialize, Deserialize,  PartialEq, Debug)]
+    pub struct SwapInfo {
+        pub result: SwapResult,
+    }
+    
+    #[derive(Serialize, Deserialize,  PartialEq, Debug)]
+    pub struct SwapResult {
+        pub return_amount: Uint128,
+        pub spread_amount: Uint128,
+    }
+    
+}
+

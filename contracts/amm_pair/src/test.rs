b082685b37970dbf8d314ba8a4b41b538f8f94d4
Michal Sivak <sivakmichal@gmail.com>
2022-04-27 00:36:35 +1000

    add unit test for init_config

--- contracts/amm_pair/src/test.rs
+++ contracts/amm_pair/src/test.rs
@@ -20,70 +20,91 @@ use shadeswap_shared::{
     },
  
 };
+
+use shadeswap_shared::{
+    fadroma::{
+        scrt::{
+            testing::{mock_dependencies, mock_env, MockApi, MockQuerier, MockStorage},
+        },
+    }
+};
+
 use composable_snip20::msg::{{InitMsg as Snip20ComposableMsg, InitConfig as Snip20ComposableConfig}};
 
 #[cfg(test)]
-mod amm_pair_test_contract{
+mod amm_pair_test_contract {
     use super::*;
+    use crate::contract::init;
 
     #[test]
-    fn init_ok() -> StdResult<()> {
-        let ref mut deps = mkdeps();
-        let env = mkenv("test");
-        let config = mkinitconfig(0);
-        assert!(init(deps, env, (&config).into()).is_ok());
-        assert_eq!(config, config_read(deps)?);
-        Err(())
-    }
+    fn assert_init_config() -> StdResult<()> {       
+        // let info = mock_info("amm_pair_contract", &amount);
+        let ref mut deps = mock_dependencies(8, &[]);
+        let mut env = mock_env(HumanAddr(String::from("TESTADDRESS")), &[]);
+        env.block.height = 200_000;
+        let amm_pair =  TokenPair(
+            TokenType::CustomToken {
+                contract_addr: HumanAddr("TOKEN0".to_string()),
+                token_code_hash: "TOKEN0_HASH".to_string()
+            },            
+            TokenType::CustomToken {
+                    contract_addr: HumanAddr("TOKEN1".to_string()),
+                    token_code_hash: "TOKEN1_HASH".to_string()
+                }
+        );
 
-    #[test]
-    fn assert_init_config() -> StdResult<()>{
-        let config = mkinitconfig(0);
-        let msg = InitMsg{
-            pair: TokenPair<HumanAddr>{
-                TokenType<HumanAddr>{
-                    contract_addr: HumanAddr("TOKEN0"),
-                    token_code_hash: "TOKEN0_HASH"
-                },
-                TokenType<HumanAddr>{
-                    contract_addr: HumanAddr("TOKEN1"),
-                    token_code_hash: "TOKEN1_HASH"
-                },
-            },
+        let msg = InitMsg {
+            pair: amm_pair,
             lp_token_contract: ContractInstantiationInfo{
-                  code_hash: "CODE_HASH",
+                  code_hash: "CODE_HASH".to_string(),
                   id :0
             },
-            factory_info: ContractLink{
-                address: HumanAddr("FACTORYADDR"),
-                code_hash: "FACTORYADDR_HASH"
+            factory_info: ContractLink {
+                address: HumanAddr(String::from("FACTORYADDR")),
+                code_hash: "FACTORYADDR_HASH".to_string()
             },
-            prng_seed: to_binary("FSDFSDFSDFSDF"),
-            entrory: to_binary("REWRQWERWERWER"),
-            callback: Callback<HumanAddr>{
-                address: HumanAddr("CALLBACKADDR"),
-                msg: to_binary(Snip20)
+            prng_seed: to_binary(&"FSDFSDFSDFSDF".to_string())?,
+            entropy: to_binary(&"REWRQWERWERWER".to_string())?,
+            callback: Callback {
+                contract: ContractLink {
+                    address: HumanAddr(String::from("CALLBACKADDR")),
+                    code_hash: "Test".to_string()
+                },
+                msg: to_binary(&String::from("Welcome bytes"))?
             },
-            symbol: "WETH"
-
-        }; 
-        let result = init(deps, env, &msg);
-        assert!(result)
+            symbol: "WETH".to_string(),
+        };
+        let result = init(deps, env.clone(), msg);
+        assert!(result.is_ok());
         Ok(())
     }
 
 
-    fn mkinitconfig(id: u64) -> Config<HumanAddr> {
-        Config::init(InitMsg {
-            pair_contract: ContractInstantiationInfo {
-                id,
-                code_hash: "2341586789".into(),
-            },
-            amm_settings: AMMSettings {
-                lp: Fee::new(28, 10000),
-                shadeswap_fee: Fee::new(2, 10000),
-                shadeswap_burner: None,
-            },
-        })
-    }
+    // fn mkinitconfig(id: u64) -> Config<HumanAddr> {
+    //     Config::init(InitMsg {
+    //         pair_contract: ContractInstantiationInfo {
+    //             id,
+    //             code_hash: "2341586789".into(),
+    //         },
+    //         amm_settings: AMMSettings {
+    //             lp: Fee::new(28, 10000),
+    //             shadeswap_fee: Fee::new(2, 10000),
+    //             shadeswap_burner: None,
+    //         },
+    //     })
+    // }
 }
+
+pub fn mock_env() -> Env {
+    Env {
+      block: BlockInfo {
+        height: 12_345,
+        time: 1_571_797_419,
+        time_nanos: 879305533,
+        chain_id: "cosmos-testnet-14002".to_string(),
+      },
+      contract: ContractInfo {
+        address: HumanAddr::from(MOCK_CONTRACT_ADDR),
+      },
+    }
+  }

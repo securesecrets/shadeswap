1c3ce4120f380e89041405f90dda02c42dfe420d
Tony <plutonyium@gmail.com>
2022-05-12 13:56:54 +1000

    Update routers

--- contracts/router/src/msg.rs
+++ contracts/router/src/msg.rs
@@ -1,15 +1,23 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use shadeswap_shared::fadroma::{ContractLink, HumanAddr};
+use shadeswap_shared::{fadroma::{ContractLink, HumanAddr, Uint128, Binary}, TokenAmount, TokenType};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub factory_address: ContractLink<HumanAddr>
+    pub factory_address: ContractLink<HumanAddr>,
+    pub prng_seed: Binary,
+    pub entropy: Binary
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
+    SwapTokens {
+        /// The token type to swap from.
+        offer: TokenAmount<HumanAddr>,
+        expected_return: Option<Uint128>,
+        path: Vec<TokenType<HumanAddr>>
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]

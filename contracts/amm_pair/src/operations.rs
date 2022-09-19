6c7f15c4a1c7d710b0a6ce727c6ed7c9d17dc724
Michal  Sivak <michal.sivak@lumachain.io>
2022-09-19 22:50:45 +1000

    fix unit test & load to may_load

--- contracts/amm_pair/src/operations.rs
+++ contracts/amm_pair/src/operations.rs
@@ -21,7 +21,7 @@ use shadeswap_shared::{
     },
     snip20::{
         helpers::{
-            burn_msg, mint_msg, register_receive, send_msg, set_viewing_key_msg, token_info,
+            burn_msg, mint_msg, register_receive, send_msg, token_info, set_viewing_key_msg,
         },
         ExecuteMsg as SNIP20ExecuteMsg,
     },
@@ -39,7 +39,10 @@ use crate::{
 
 // WHITELIST
 pub fn add_whitelist_address(storage: &mut dyn Storage, address: Addr) -> StdResult<()> {
-    let mut unwrap_data = whitelist_r(storage).load()?;
+    let mut unwrap_data = match whitelist_r(storage).may_load(){
+        Ok(v) => v.unwrap_or(Vec::new()),
+        Err(err) => Vec::new(),
+    };
     unwrap_data.push(address);
     whitelist_w(storage).save(&unwrap_data)
 }
@@ -56,7 +59,10 @@ pub fn remove_whitelist_address(
 }
 
 pub fn is_address_in_whitelist(storage: &dyn Storage, address: Addr) -> StdResult<bool> {
-    let addrs = whitelist_r(storage).load()?;
+    let addrs = match whitelist_r(storage).may_load() {
+        Ok(it) => it.unwrap_or(Vec::new()),
+        Err(err) => Vec::new(),
+    };
     if addrs.contains(&address) {
         return Ok(true);
     } else {
@@ -71,7 +77,10 @@ fn load_trade_history(deps: Deps, count: u64) -> StdResult<TradeHistory> {
 }
 
 fn store_trade_history(deps: DepsMut, trade_history: &TradeHistory) -> StdResult<()> {
-    let count = trade_count_r(deps.storage).load()?;
+    let count: u64 = match trade_count_r(deps.storage).may_load() {
+        Ok(it) => it.unwrap_or(0),
+        Err(err) => 0,
+    };
     let update_count = count + 1;
     trade_count_w(deps.storage).save(&update_count)?;
     trade_history_w(deps.storage).save(update_count.to_string().as_bytes(), &trade_history)
@@ -477,7 +486,7 @@ pub fn calculate_swap_result(
     let mut lp_fee_amount = Uint128::zero();
     let mut shade_dao_fee_amount = Uint128::zero();
     // calculation fee
-    let discount_fee = is_address_in_whitelist(deps.storage, recipient)?;
+    let discount_fee =  is_address_in_whitelist(deps.storage, recipient)?;
     if discount_fee == false {
         match &config.custom_fee {
             Some(f) => {
